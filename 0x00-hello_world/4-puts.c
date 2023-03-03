be06d2d638cdb3f406534085cf400c56a7929252
Yisrayl Udeh <Yisrayludeh1@gmail.com>
2023-03-03 04:45:21 -0800

    Hello,printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line puts function
+ * main - A program
  * Return: 0 (Sucess)
  */
-int main(void)
+iny main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
